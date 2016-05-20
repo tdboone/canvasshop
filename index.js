54b1a85f337a73506f7a4384166ac8df2ee16a42
tdboone <tdboone@gmail.com>
2016-05-20 15:54:15 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    applyDifferenceImageUsingMap : require('./lib/apply-difference-image-using-map'),
+    concatenateImagesIntoGridImage : require('./lib/concatenate-images-into-grid-image'),
+    convertDifferenceImageToSolidColorMap : require('./lib/convert-difference-image-to-solid-color-map'),
+    generateShadingImageFromLightingComponents : require('./lib/generate-shading-image-from-lighting-components'),
+    getImageWithOnlyChangedPixels : require('./lib/get-image-with-only-changed-pixels'),
+    replaceRGBChannelsWithColors : require('./lib/replace-rgb-channels-with-colors'),
+    separateGridImageIntoArray : require('./lib/separate-grid-image-into-array')
+};
